062e2b9bcc287cf5c3fdda844d8f52d3fcc8c976
Israel Omola <izzyskills@gmail.com>
2024-09-15 11:48:16 +0100

    added frontend

---
+++ VidConfFront/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheWelcome from "../components/TheWelcome.vue";
+import { Button } from "@/components/ui/button";
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <Button>click me</Button>
+  </main>
+</template>
