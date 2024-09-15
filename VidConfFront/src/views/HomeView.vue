e0c2ea78dd7976439f39186d108a56d69491d555
Israel Omola <izzyskills@gmail.com>
2024-09-15 11:59:31 +0100

    added darkmode

--- VidConfFront/src/views/HomeView.vue
+++ VidConfFront/src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import TheWelcome from "../components/TheWelcome.vue";
 import { Button } from "@/components/ui/button";
+import DarkMode from "@/components/DarkMode.vue";
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <DarkMode />
     <Button>click me</Button>
   </main>
 </template>
