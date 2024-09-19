c6e447106e500d83cb1c18d813f3f963f1b5bf86
Israel Omola <izzyskills@gmail.com>
2024-09-19 20:18:11 +0100

    login backend

---
+++ Vidconf/src/db/models.py
@@ -0,0 +1,25 @@
+import uuid
+from datetime import date, datetime
+from typing import List, Optional
+
+import sqlalchemy.dialects.postgresql as pg
+from sqlmodel import Column, Field, Relationship, SQLModel
+
+
+class User(SQLModel, table=true):
+    uid: uuid.UUID = Field(
+        sa_column=Column(pg.UUID, nullable=False, primary_key=True, default=uuid.uuid4)
+    )
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    role: str = Field(
+        sa_column=Column(pg.VARCHAR, nullable=False, server_default="user")
+    )
+    is_verified: bool = Field(default=False)
+    password_hash: str = Field(
+        sa_column=Column(pg.VARCHAR, nullable=True), exclude=True
+    )
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    update_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
