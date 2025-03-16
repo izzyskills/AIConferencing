f4d5b8a623f129362d81bcad65c91b113ff15d87
Israel Omola <izzyskills@gmail.com>
2025-03-16 12:16:30 +0100

    migrations

--- Vidconf/src/db/main.py
+++ Vidconf/src/db/main.py
@@ -2,15 +2,22 @@ from sqlalchemy.ext.asyncio import AsyncEngine
 from sqlalchemy.orm import sessionmaker
 from sqlmodel import SQLModel, create_engine
 from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlmodel import Session
 
 from src.config import Config
 
 async_engine = AsyncEngine(create_engine(url=Config.DATABASE_URL))
+sync_engine = create_engine(url=Config.DATABASE_URL_SYNC)
 
 
 async def init_db() -> None:
     async with async_engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
+
+
+def get_sync_session():
+
+    return Session(sync_engine)
 
 
 async def get_session() -> AsyncSession:
